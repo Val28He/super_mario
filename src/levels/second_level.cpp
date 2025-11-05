2a17620dd69054915006592ac0695706a1a50694
Val28He <bogdanovavalentina0405@gmail.com>
2025-11-05 18:57:03 +0300

    1 пункт
    Добавлены элементы во второй уровень

--- src/levels/second_level.cpp
+++ src/levels/second_level.cpp
@@ -23,7 +23,22 @@ void SecondLevel::init_data() {
 	ui_factory->create_ship({20, 25}, 40, 2);
 	ui_factory->create_ship({60, 20}, 10, 7);
 	ui_factory->create_ship({80, 25}, 20, 2);
+	
+	ui_factory->create_full_box({40, 15}, 5, 3);
+	ui_factory->create_full_box({120, 10}, 5, 3);
+	ui_factory->create_full_box({180, 15}, 5, 3);
+	
 	ui_factory->create_ship({120, 20}, 10, 7);
 	ui_factory->create_ship({150, 25}, 40, 2);
 	ui_factory->create_ship({210, 20}, 10, 7);
+	
+	ui_factory->create_enemy({25, 5}, 3, 2);
+	ui_factory->create_enemy({30, 5}, 3, 2);
+	ui_factory->create_enemy({40, 5}, 3, 2);
+	ui_factory->create_enemy({80, 5}, 3, 2);
+	ui_factory->create_enemy({85, 5}, 3, 2);
+	ui_factory->create_enemy({90, 5}, 3, 2);
+	ui_factory->create_enemy({110, 5}, 3, 2);
+	ui_factory->create_enemy({160, 5}, 3, 2);
+	ui_factory->create_enemy({180, 5}, 3, 2);
 }
