116ae378d797a9ef14999b4ca5a2f7199f06e20c
Val28He <bogdanovavalentina0405@gmail.com>
2025-11-05 18:59:32 +0300

    Update second_level.hpp

--- src/levels/second_level.hpp
+++ src/levels/second_level.hpp
@@ -8,7 +8,6 @@ namespace biv {
 			SecondLevel(UIFactory* ui_factory);
 			
 			GameLevel* get_next() override;
-			bool is_final() const noexcept override;
 			
 		private:
 			void init_data() override;
