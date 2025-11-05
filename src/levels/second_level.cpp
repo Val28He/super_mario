5f4feeef0769360c64b0173646c89990bb886339
Val28He <bogdanovavalentina0405@gmail.com>
2025-11-05 18:59:07 +0300

    Update second_level.cpp

--- src/levels/second_level.cpp
+++ src/levels/second_level.cpp
@@ -1,16 +1,18 @@
 #include "second_level.hpp"
 
+#include "third_level.hpp"
+
 using biv::SecondLevel;
 
 SecondLevel::SecondLevel(UIFactory* ui_factory) : GameLevel(ui_factory) {
 	init_data();
 }
 
-bool SecondLevel::is_final() const noexcept {
-	return true;
-}
-
 biv::GameLevel* SecondLevel::get_next() {
+	if (!next) {
+		clear_data();
+		next = new biv::ThirdLevel(ui_factory);
+	}
 	return next;
 }
 
@@ -20,14 +22,13 @@ biv::GameLevel* SecondLevel::get_next() {
 void SecondLevel::init_data() {
 	ui_factory->create_mario({39, 10}, 3, 3);
 	
-	ui_factory->create_ship({20, 25}, 40, 2);
-	ui_factory->create_ship({60, 20}, 10, 7);
-	ui_factory->create_ship({80, 25}, 20, 2);
-	
 	ui_factory->create_full_box({40, 15}, 5, 3);
 	ui_factory->create_full_box({120, 10}, 5, 3);
 	ui_factory->create_full_box({180, 15}, 5, 3);
 	
+	ui_factory->create_ship({20, 25}, 40, 2);
+	ui_factory->create_ship({60, 20}, 10, 7);
+	ui_factory->create_ship({80, 25}, 20, 2);
 	ui_factory->create_ship({120, 20}, 10, 7);
 	ui_factory->create_ship({150, 25}, 40, 2);
 	ui_factory->create_ship({210, 20}, 10, 7);
